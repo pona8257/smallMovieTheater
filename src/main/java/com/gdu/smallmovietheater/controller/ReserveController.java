4faecf719a3672a2eb7785cb82f58062b5945c2d
in002ou <in002ou@gmail.com>
2023-05-25 21:20:27 +0900

    reserve

---
+++ src/main/java/com/gdu/smallmovietheater/controller/ReserveController.java
@@ -0,0 +1,72 @@
+package com.gdu.smallmovietheater.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gdu.smallmovietheater.domain.MovieDTO;
+import com.gdu.smallmovietheater.service.ReservationService;
+
+@Controller
+@RequestMapping("/movie")
+public class ReserveController {
+	
+	@Autowired
+	private ReservationService reservationService;
+	
+	@GetMapping("/reserve.form")
+	public String reserve() {
+		return "/reserve/reservation";
+	}
+	
+	@GetMapping(value="/date.do", produces="application/json")
+	@ResponseBody
+	public Map<String, Object> getDate(){
+		return reservationService.getDate();
+	}
+	
+	@GetMapping(value="/getMovieList.do", produces="application/json")
+	@ResponseBody
+	public Map<String, Object> getMovieList(){
+		return reservationService.getMovieList();
+	}
+	
+	@GetMapping(value="/reserveSelectMovie.do", produces="application/json")
+	@ResponseBody
+	public Map<String, Object> getMovieScreenDate(String title){
+		return reservationService.getScreenDate(title);
+	}
+	
+	@GetMapping(value="searchScreenDate.do", produces="application/json")
+	@ResponseBody
+	public Map<String, Object> searchScreenDateMovie(String date){
+		return reservationService.searchScreenDateMovie(date);
+	}
+	
+	@GetMapping(value="/reserveSelectTime.do", produces="application/json")
+	@ResponseBody
+	public Map<String, Object> selectMovieAndTime(HttpServletRequest request){
+		return reservationService.selectMovieByMovieAndTime(request);
+	}
+	
+	@GetMapping(value="/getSeats.do", produces="application/json")
+	@ResponseBody
+	public Map<String, Object> getSeats(HttpServletRequest request){
+		return reservationService.getScreenSeats(request);
+	}
+	
+	@PostMapping(value="/reservation.do", produces="application/json")
+	@ResponseBody
+	public void reservation(HttpServletRequest request) {
+		reservationService.reservation(request);
+	}
+	
+}
---
+++ src/main/java/com/gdu/smallmovietheater/service/ReservationService.java
@@ -0,0 +1,19 @@
+package com.gdu.smallmovietheater.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public interface ReservationService {
+	
+	public Map<String, Object> getDate();
+	public Map<String, Object> getMovieList();
+	public Map<String, Object> getScreenDate(String title);
+	public Map<String, Object> searchScreenDateMovie(String date);
+	public Map<String, Object> selectMovieByMovieAndTime(HttpServletRequest request);
+	public Map<String, Object> getScreenSeats(HttpServletRequest request);
+	public void reservation(HttpServletRequest request);
+	
+}
